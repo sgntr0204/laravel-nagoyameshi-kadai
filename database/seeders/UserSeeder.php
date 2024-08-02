b41ada467123c652ef88a02d35c85a2d5ad53c50
sgntr0204 <run.shgar.112519@gmail.com>
2024-08-02 20:44:47 +0900

    修正

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+     // 100人のユーザーを作成
+         User::factory()->count(100)->create();
+
+        // $user = new User();
+        // $user->email = 'user@example.com';
+        // $user->password = Hash::make('password');
+        // $user->save();
+    }
+}
